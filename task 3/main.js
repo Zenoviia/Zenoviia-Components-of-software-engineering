323b6a9ae401a2781e1e0abe6cd258217eecba2c
Zenoviia <zenoviatkacenko@gmail.com>
2024-12-30 15:09:35 +0200

    Clear asynvFilter function

--- task 3/main.js
+++ task 3/main.js
@@ -3,44 +3,13 @@
 
 "use strict";
 
-const asyncFilterPromise = (array, asyncFunction) => {
-  const promises = array.map((item, index) => asyncFunction(item, index));
+const asyncFilter = (array, asyncFunction) => {
 
-  return Promise.allSettled(promises).then((results) => {
-    const successfulResults = results
-      .filter((result) => result.status === "fulfilled")
-      .map((result) => result.value);
-
-    const errors = results
-      .filter((result) => result.status === "rejected")
-      .map((result) => result.reason);
-
-    if (errors.length > 0) {
-      const aggregateError = new AggregateError(
-        errors,
-        "Some promises were rejected"
-      );
-      aggregateError.successfulResults = successfulResults;
-      throw aggregateError;
-    }
-
-    return successfulResults;
-  });
-};
-
-const checkAccess = (user) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      user.hasAccess
-        ? resolve(user)
-        : reject(`User ${user.name} (ID: ${user.id}) does not have access`);
-    }, 1000);
-  });
 };
 
 const array = Array.from({ length: 100 }, (_, index) => index);
 
-asyncFilterPromise(users, checkAccess)
+asyncFilter(array, checkAccess)
   .then((result) => {
     console.log("Users with access:", result);
   })
