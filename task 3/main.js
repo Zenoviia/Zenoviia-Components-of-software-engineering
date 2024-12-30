05fb6cd5cd101dd9383f54635221ffb5834f0352
Zenoviia <zenoviatkacenko@gmail.com>
2024-12-30 15:17:53 +0200

    Clear customFilter function

--- task 3/main.js
+++ task 3/main.js
@@ -7,17 +7,14 @@ const asyncFilter = (array, asyncFunction) => {
 
 };
 
-const array = Array.from({ length: 100 }, (_, index) => index);
+const customFilter = (item) => {
+    return item;
+}
 
-asyncFilter(array, checkAccess)
-  .then((result) => {
-    console.log("Users with access:", result);
-  })
-  .catch((error) => {
-    if (error instanceof AggregateError) {
-      console.error("Errors occurred:", error.errors); // Перелік помилок
-      console.log("Users with access:", error.successfulResults); // Успішні результати
-    } else {
-      console.error("An unexpected error occurred:", error);
-    }
-  });
+const array = Array.from({length: 100}, (_, index) => index);
+
+asyncFilter(array, async (item, signal) => {
+    if (signal.aborted) {
+        throw new Error(`Aborted before processing element "${item}"!`);
+    } else return customFilter(item);
+})
