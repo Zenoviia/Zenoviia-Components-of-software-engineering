080e8240bd3aeebf014a815668ed6986912be766
Zenoviia <zenoviatkacenko@gmail.com>
2024-12-30 12:17:48 +0200

    Remove users with access output

--- task 3/main.js
+++ task 3/main.js
@@ -61,18 +61,13 @@ const checkAccess = (user, index, signal) => {
 const controller = new AbortController();
 const { signal } = controller;
 
-asyncFilterPromise(users, checkAccess, signal)
-  .then((result) => {
-    console.log("Users with access:", result);
-  })
-  .catch((error) => {
-    if (error instanceof AggregateError) {
-      console.error("Errors occurred:", error.errors); // Перелік помилок
-      console.log("Users with access:", error.successfulResults); // Успішні результати
-    } else {
-      console.error("An unexpected error occurred:", error);
-    }
-  });
+asyncFilterPromise(users, checkAccess, signal).catch((error) => {
+  if (error instanceof AggregateError) {
+    console.error("Errors occurred:", error.errors); // Перелік помилок
+  } else {
+    console.error("An unexpected error occurred:", error);
+  }
+});
 
 setTimeout(() => {
   controller.abort();
