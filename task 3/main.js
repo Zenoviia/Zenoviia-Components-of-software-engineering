8f3687e8cf9cdb7f9999fd45968d0241f80875c5
Zenoviia <zenoviatkacenko@gmail.com>
2024-12-30 19:31:31 +0200

    Update code

--- task 3/main.js
+++ task 3/main.js
@@ -10,25 +10,25 @@ const asyncFilter = async (array, asyncFunction) => {
 
     setTimeout(() => {
         controller.abort();
-    }, 5000);
+    }, 500);
 
     for (const element of array) {
         if (signal.aborted) {
             throw new Error(`Aborted before processing element "${element}"!`);
         }
-        console.log("Processed Element:", element);
+        console.log("Generated element:", element);
         if (await asyncFunction(element, signal)) {
             results.push(element);
         }
 
-        await new Promise((resolve) => setTimeout(resolve, 70));
+        await new Promise((resolve) => setTimeout(resolve, 30));
     }
     return {results};
-}
+};
 
 const customFilter = (item) => {
     return item % 10 === 0;
-}
+};
 
 const array = Array.from({length: 100}, (_, index) => index);
 
@@ -36,5 +36,6 @@ asyncFilter(array, async (item, signal) => {
     if (signal.aborted) {
         throw new Error(`Aborted before processing element "${item}"!`);
     } else return customFilter(item);
-}).then((res) => console.log("Filtered array: ", res))
-    .catch((err) => console.log(err))
+})
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
